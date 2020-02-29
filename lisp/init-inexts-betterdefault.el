52c309d0278651c6348a5eab0f2f0016df527fde
inextsEthereal <inexts.ethereal@gmail.com>
2020-02-29 23:31:22 +0800

    add config

---
+++ lisp/init-inexts-betterdefault.el
@@ -0,0 +1,6 @@
+(set-frame-font "Ubuntu Mono-13" nil t)
+(setq make-backup-files nil)
+(setq delete-trailing-whitespace t)
+(whitespace-cleanup)
+
+(provide 'init-inexts-betterdefault)
---
+++ lisp/init-local.el
@@ -0,0 +1,11 @@
+
+(require 'init-inexts-elpa)
+
+;;----------------------------------------------------------------------------
+;; Require setting of packages
+;;----------------------------------------------------------------------------
+(require 'init-evil)
+
+(require 'init-inexts-betterdefault)
+
+(provide 'init-local)
