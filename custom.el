59c3433918c23f8f67ea14070a8e329a8f995eeb
zhouzigui <zhouzigui@huixuanjiasu.com>
2023-08-22 20:06:07 +0800

    modify config

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(sanityinc-tomorrow-bright))
+ '(custom-safe-themes
+   '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default))
+ '(inhibit-startup-screen t)
+ '(global-display-line-numbers-mode t)
+ '(package-selected-packages
+   '(consult embark vertico orderless marginalia lsp-mode keycast gruvbox-theme embark-consult company color-theme-sanityinc-tomorrow))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack Nerd Font Mono" :foundry "outline" :slant normal :weight regular :height 113 :width normal)))))
--- init.el
+++ init.el
@@ -1,19 +1,19 @@
 ;;在文件最开头添加地个 文件作用域的变量设置，设置变量的绑定方式
 ;; -*- lexical-binding: t -*-
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+(setq-default initial-scratch-message
+              (concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
 (setq tab-always-indent 'complete)
-(setq w32-apps-modifier 'super)
 
 (server-mode 1)
 
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
 (menu-bar-mode 0)
-;;(icomplete-mode 1)
+
 (global-display-line-numbers-mode 1)
 (electric-pair-mode 1)
-;;(toggle-frame-fullscreen)
-;;(toggle-frame-maximized)
-;;(setq cursor-type 'box)
 
 (defun open-init-file()
   (interactive)
@@ -21,13 +21,6 @@
 
   (global-set-key (kbd "<f2>") 'open-init-file)
 
-
-(global-set-key (kbd "s-a") 'mark-whole-buffer) ;;对应Windows上面的Ctrl-a 全选
-(global-set-key (kbd "s-c") 'kill-ring-save) ;;对应Windows上面的Ctrl-c 复制
-(global-set-key (kbd "s-s") 'save-buffer) ;; 对应Windows上面的Ctrl-s 保存
-(global-set-key (kbd "s-v") 'yank) ;对应Windows上面的Ctrl-v 粘贴
-(global-set-key (kbd "s-z") 'undo) ;对应Windows上面的Ctrol-z 撤销
-(global-set-key (kbd "s-x") 'kill-region) ;对应Windows上面的Ctrol-x 剪切
 ;;让鼠标滚动更好用
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control) . nil)))
 (setq mouse-wheel-progressive-speed nil)
@@ -47,10 +40,6 @@
 (global-company-mode 1)
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
 (define-key company-active-map (kbd "C-n") 'company-select-next)
-
-;;modeline上显示我的所有的按键和执行的命令
-;;(package-install 'keycast)
-;;(keycast-mode-line-mode t)
 
 (package-install 'vertico)
 (vertico-mode t)
@@ -78,22 +67,10 @@
 (setq make-backup-files nil)
 (require 'recentf)
 (recentf-mode 1)
+(setq recentf-max-menu-items 10)
+(delete-selection-mode t)
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(gruvbox-dark-hard))
- '(custom-safe-themes
-   '("d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
- '(package-selected-packages
-   '(color-theme-sanityinc-tomorrow gruvbox-theme embark-consult consult embark orderless marginalia vertico keycast company)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack Nerd Font Mono" :foundry "outline" :slant normal :weight regular :height 113 :width normal)))))
-(put 'upcase-region 'disabled nil)
+(global-set-key (kbd "C-h C-f") 'find-function)
+(global-set-key (kbd "C-h C-v") 'find-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
+;;(setq inhibit-startup-screen t)
