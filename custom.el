46153cde29de5e67e014dde8daa6a591c706d14f
zhouzigui <zhouzigui@huixuanjiasu.com>
2023-08-25 19:41:56 +0800

    add custom.el

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruvbox-glass))
+ '(custom-safe-themes
+   '("d738f1914c16f245a16fbbd3f7699119bf278f1b34a3f1d8ee2387d0ca1e1c38" "0b6b1a4a576372ba240341912d06fc60e4a5088e51babbafb4252b2cdbf9fa00" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default))
+ '(display-line-numbers-type 'relative)
+ '(global-display-line-numbers-mode t)
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   '(dimmer color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized gnu-elpa-keyring-update fullframe seq mode-line-bell wgrep consult embark vertico orderless marginalia lsp-mode keycast gruvbox-theme embark-consult company))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack Nerd Font Mono" :foundry "outline" :slant normal :weight regular :height 113 :width normal)))))
