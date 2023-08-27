8aa5e7bbfffa725e346290dfba6b7142b3aedb6c
zhouzigui <zhouzigui@huixuanjiasu.com>
2023-08-27 12:29:30 +0800

    modify init.el

--- init.el
+++ init.el
@@ -10,11 +10,11 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
 
-(let ((normal-gc-cons-threshold (* 20 1024 1024))
-      (init-gc-cons-threshold (* 128 1024 1024)))
-  (setq gc-cons-threshold init-gc-cons-threshold)
-  (add-hook 'emacs-startup-hook
-            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+;;(let ((normal-gc-cons-threshold (* 20 1024 1024))
+;;      (init-gc-cons-threshold (* 128 1024 1024)))
+;;  (setq gc-cons-threshold init-gc-cons-threshold)
+;;  (add-hook 'emacs-startup-hook
+;;            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold)));;)
 
 (setq custom-file (locate-user-emacs-file "custom.el"))
 (when (file-exists-p custom-file)
