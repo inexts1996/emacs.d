8204e02c938ca9be94dd5ae67a3f93d6ca6b15fd
zhouzigui <zhouzigui@huixuanjiasu.com>
2023-09-24 14:56:38 +0800

    configs

--- init.el
+++ init.el
@@ -1,56 +1,91 @@
-;;在文件最开头添加地个 文件作用域的变量设置，设置变量的绑定方式
 ;; -*- lexical-binding: t -*-
-(let ((minver "26.1"))
-  (when (version< emacs-version minver)
-    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+;;(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(setq w32-apps-modifier 'super)       ; 通过SharpKeys改成了 Application
+(global-set-key (kbd "s-a") 'mark-whole-buffer) ;;对应Windows上面的Ctrl-a 全选
+(global-set-key (kbd "s-c") 'kill-ring-save) ;;对应Windows上面的Ctrl-c 复制
+(global-set-key (kbd "s-s") 'save-buffer) ;; 对应Windows上面的Ctrl-s 保存
+(global-set-key (kbd "s-v") 'yank) ;对应Windows上面的Ctrl-v 粘贴
+(global-set-key (kbd "s-z") 'undo) ;对应Windows上面的Ctrol-z 撤销
+(global-set-key (kbd "s-x") 'kill-region) ;对应Windows上面的Ctrol-x 剪切
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(sanityinc-tomorrow-bright))
+ '(custom-safe-themes
+   '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default))
+ '(package-selected-packages
+   '(page-break-lines rainbow-delimiters mode-line-bell color-theme-sanityinc-tomorrow embark-consult consult embark marginalia orderless vertico company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack Nerd Font Propo" :foundry "outline" :slant normal :weight regular :height 120 :width normal)))))
 
-(when (version< emacs-version "27.1")
-(message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
+;; emacs 相关配置
+(require 'recentf)
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-(add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
-
-;;(let ((normal-gc-cons-threshold (* 20 1024 1024))
-;;      (init-gc-cons-threshold (* 128 1024 1024)))
-;;  (setq gc-cons-threshold init-gc-cons-threshold)
-;;  (add-hook 'emacs-startup-hook
-;;            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold)));;)
-
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(when (file-exists-p custom-file)
-  (load custom-file)
-  )
-
-(setq-default initial-scratch-message
-              (concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
+(setq-default display-line-numbers-type 'relative)
+(setq-default display-line-numbers-width 3)
+(setq inhibit-startup-screen t)
 (setq tab-always-indent 'complete)
-
-(server-mode 1)
-
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
-(menu-bar-mode 0)
-
-(global-display-line-numbers-mode 1)
-(electric-pair-mode 1)
-
-(defun open-init-file()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
-
-  (global-set-key (kbd "<f2>") 'open-init-file)
-
 ;;让鼠标滚动更好用
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control) . nil)))
 (setq mouse-wheel-progressive-speed nil)
-(require 'init-elpa)
-;;(require 'init-packages)
-(require 'init-themes)
-(require 'init-org)
-(package-install 'company)
-(global-company-mode 1)
-(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(setq make-backup-files nil)
+(setq recentf-max-menu-items 10)
+
+(recentf-mode t)
+(global-display-line-numbers-mode t)
+(server-mode t)
+;;(icomplete-mode t)
+(electric-pair-mode t)
+(show-paren-mode t)
+(delete-selection-mode t)
+(global-hl-line-mode t)
+(global-display-fill-column-indicator-mode t)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq-default indicate-buffer-boundaries 'left)
+(setq-default display-fill-column-indicator-character ?\u254e)
+
+(global-set-key (kbd "C-x b") 'consult-buffer)
+(global-set-key (kbd "C-h C-f") 'find-function)
+(global-set-key (kbd "C-h C-v") 'find-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
+
+;; company config
+(global-company-mode t)
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 1)
 (define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+
+;; 自定义函数
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el")
+  )
+(global-set-key (kbd "<f2>") 'open-init-file)
+
+;; 包管理及安装
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;;(setq package-archives '(("gnu"   . "http://elpa.zilongshanren.com/gnu/")
+;;
+;;			 ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+;;防止反复调用 package-refresh-contents 会影响加载速度
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 (package-install 'vertico)
 (vertico-mode t)
@@ -64,24 +99,26 @@
 (package-install 'embark)
 (global-set-key (kbd "C-;") 'embark-act)
 (setq prefix-help-command 'embark-prefix-help-command)
+
 (package-install 'embark-consult)
+
 (package-install 'consult)
 (global-set-key (kbd "C-s") 'consult-line)
-(global-set-key (kbd "M-s i") 'consult-imenu)
+(global-set-key (kbd "C-c i") 'consult-imenu)
 
-(show-paren-mode t)
-(setq company-minimum-prefix-length 1)
-(setq company-idle-delay 0)
-(setq make-backup-files nil)
-(require 'recentf)
-(recentf-mode 1)
-(setq recentf-max-menu-items 10)
-(delete-selection-mode t)
+(package-install 'color-theme-sanityinc-tomorrow)
+(require 'color-theme-sanityinc-tomorrow t)
 
-(global-set-key (kbd "C-h C-f") 'find-function)
-(global-set-key (kbd "C-h C-v") 'find-variable)
-(global-set-key (kbd "C-h C-k") 'find-function-on-key)
+(package-install 'mode-line-bell)
+(mode-line-bell-mode t)
 
+(package-install 'rainbow-delimiters)
+(rainbow-delimiters-mode t)
+
+(package-install 'page-break-lines)
+(global-page-break-lines-mode t)
+
+;; emax config
 (progn
   (defvar emax-root (concat (expand-file-name "~") "/emax"))
   (defvar emax-bin (concat emax-root "/bin"))
@@ -99,99 +136,8 @@
 
   ;;可选安装msys64
   ;;下载地址: http://repo.msys2.org/mingw/sources/
-  (setenv "PATH" (concat "D:\\Program Files\\Scoop\\UserApps\\apps\\msys2\\current\\mingw64\\bin;" (getenv "PATH")))
+  (setenv "PATH" (concat "C:\\msys64\\usr\\bin;C:\\msys64\\mingw64\\bin;" (getenv "PATH")))
 
   ;; (dolist (dir '("~/emax/" "~/emax/bin/" "~/emax/bin64/" "~/emax/lisp/" "~/emax/elpa/"))
   ;;   (add-to-list 'load-path dir))
   )
-(global-hl-line-mode t)
-
-(package-install 'wgrep)
-(setq wgrep-auto-save-buffer t)
-(eval-after-load
-    'consult
-  '(eval-after-load
-       'embark
-     '(progn
-	(require 'embark-consult)
-	(add-hook
-	 'embark-collect-mode-hook
-	 #'consult-preview-at-point-mode))))
-
-(defun embark-export-write ()
-  "Export the current vertico results to a writable buffer if possible.
-Supports exporting consult-grep to wgrep, file to wdeired, and consult-location to occur-edit"
-  (interactive)
-  (require 'embark)
-  (require 'wgrep)
-  (pcase-let ((`(,type . ,candidates)
-               (run-hook-with-args-until-success 'embark-candidate-collectors)))
-    (pcase type
-      ('consult-grep (let ((embark-after-export-hook #'wgrep-change-to-wgrep-mode))
-                       (embark-export)))
-      ('file (let ((embark-after-export-hook #'wdired-change-to-wdired-mode))
-               (embark-export)))
-      ('consult-location (let ((embark-after-export-hook #'occur-edit-mode))
-                           (embark-export)))
-      (x (user-error "embark category %S doesn't support writable export" x)))))
-
-(define-key minibuffer-local-map (kbd "C-c C-e") 'embark-export-write)
-
-
-;; 配置搜索中文
-(progn
-  (setq consult-locate-args (encode-coding-string "es.exe -i -p -r" 'gbk))
-  (add-to-list 'process-coding-system-alist '("es" gbk . gbk))
-  )
-(eval-after-load 'consult
-  (progn
-    (setq
-     consult-narrow-key "<"
-     consult-line-numbers-widen t
-     consult-async-min-input 2
-     consult-async-refresh-delay  0.15
-     consult-async-input-throttle 0.2
-     consult-async-input-debounce 0.1)
-    ))
-
-(package-install 'pyim)
-
-(defun eh-orderless-regexp (orig_func component)
-  (let ((result (funcall orig_func component)))
-    (pyim-cregexp-build result)))
-
-
-(defun toggle-chinese-search ()
-  (interactive)
-  (if (not (advice-member-p #'eh-orderless-regexp 'orderless-regexp))
-      (advice-add 'orderless-regexp :around #'eh-orderless-regexp)
-    (advice-remove 'orderless-regexp #'eh-orderless-regexp)))
-
-(defun disable-py-search (&optional args)
-  (if (advice-member-p #'eh-orderless-regexp 'orderless-regexp)
-      (advice-remove 'orderless-regexp #'eh-orderless-regexp)))
-
-;; (advice-add 'exit-minibuffer :after #'disable-py-search)
-(add-hook 'minibuffer-exit-hook 'disable-py-search)
-
-(global-set-key (kbd "s-p") 'toggle-chinese-search)
-
-(package-install 'mode-line-bell)
-(mode-line-bell-mode)
-(global-visual-line-mode t)
-;; 设置英文字体
-(set-face-attribute
- 'default nil
- :font (font-spec :name "Hack Nerd Font Mono"
-                  :weight 'normal
-                  :slant 'normal
-                  :size 11.0))
-
-;; 设置中文字体
-(dolist (charset '(kana han symbol cjk-misc bopomofo))
-  (set-fontset-font (frame-parameter nil 'font)
-                    charset
-                    (font-spec :name "-outline-微软雅黑-normal-normal-normal-sans-18-*-*-*-p-*-iso8859-1"
-                               :weight 'normal
-                               :slant 'normal
-                               :size 12.0)))
--- lisp/init-org.el
+++
@@ -1,5 +0,0 @@
-(require 'org-tempo)
-(with-eval-after-load 'org
-(add-to-list 'org-modules 'org-habit)
-  )
-(provide 'init-org)
